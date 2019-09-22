da54d4b0071c944bdc224b5ff1425f42ff5961ac
Alex Harvey <alexharv074@gmail.com>
2019-09-22 21:38:57 +1000

    Add types into variables.tf
    This is needed both because it's good to declare type constraints, and
    also because my auto docs generation assumes you have declared type
    constraints.

--- variables.tf
+++ variables.tf
@@ -1,19 +1,24 @@
 variable "client_name" {
+  type        = string
   description = "Name of the organisation, used in the bucket name to ensure there are no conflicts"
 }
 
 variable "global_state_bucket" {
+  type        = string
   description = "The name of the bucket containing the global module state"
 }
 
 variable "global_state_key" {
+  type        = string
   description = "The key of the global module state as defined in the backend"
 }
 
 variable "global_state_region" {
+  type        = string
   description = "The region of the bucket containing the global module state"
 }
 
 variable "global_state_profile" {
+  type        = string
   description = "The profile to be used to access the global module state bucket"
 }
